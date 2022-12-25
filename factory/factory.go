7dbffe52b93d7589452b41cd558b29d139fe8eca
xiaolo66 <354159177@qq.com>
2022-12-25 22:03:39 +0800

    factory

--- factory/factory.go
+++ factory/factory.go
@@ -3,6 +3,7 @@ package factory
 import (
 	"github.com/xiaolo66/ExchangeApi"
 	"github.com/xiaolo66/ExchangeApi/exchanges/binance"
+	"github.com/xiaolo66/ExchangeApi/exchanges/huobi"
 	"github.com/xiaolo66/ExchangeApi/exchanges/okex"
 )
 
@@ -14,8 +15,8 @@ func NewExchange(t ExchangeApi.ExchangeType, option ExchangeApi.Options) Exchang
 	//	return zb.New(option)
 	case ExchangeApi.Okex:
 		return okex.New(option)
-	//case ExchangeApi.Huobi:
-	//	return huobi.New(option)
+	case ExchangeApi.Huobi:
+		return huobi.New(option)
 	//case ExchangeApi.GateIo:
 	//	return gateio.New(option)
 	}
